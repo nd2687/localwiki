383e59246cfc16622428e88061c7e106a6271cb7
Takashi Kawano <kawano.fanta@gmail.com>
2015-12-11 03:56:22 +0900

    Resize textarea

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+$(function(){
+  var ta = $(".item-description");
+  var edit = $("#item-ta-edit");
+  if(ta.length == 0){ return; }
+
+  ta.height(50);
+  ta.css("lineHeight", "20px");
+
+  resize(ta);
+
+  if(edit.length){
+    var scrollHeight = ta.get(0).scrollHeight;
+    var offsetHeight = ta.get(0).offsetHeight;
+
+    if(scrollHeight > offsetHeight){
+      ta.height(scrollHeight);
+    } else {
+      var lineHeight = Number(ta.css("lineHeight").split("px")[0]);
+      while (true){
+        ta.height(ta.height() - lineHeight);
+        if(scrollHeight > offsetHeight){
+          ta.height(scrollHeight);
+          break;
+        }
+      }
+    }
+  }
+});
+
+function resize(textarea){
+  textarea.on("keyup", function(){
+    var tval = textarea.val();
+    var num = tval.match(/\n|\r\n/g);
+
+    if(tval == "" || num == null){ return; }
+    if(num != null){ var len = num.length + 1; }
+    len = len + 1;
+    textarea.css('height', len * 1.2 + "em");
+  })
+}
