3ad60994d9a1464d127aa4e5c4b12b727e1a1a44
Takashi Kawano <kawano.fanta@gmail.com>
2015-12-14 01:26:48 +0900

    fix next to return

--- class/user_csv.rb
+++ class/user_csv.rb
@@ -5,8 +5,8 @@ class UserCsv
   class << self
     def add(name)
       CSV.open("localwiki.csv", "a:UTF-8") do |csv|
-        next if name =~ %r(^[+-.$()?*/&%!"'_,]+)
-        next if name =~ /^[-.0-9]+$/
+        return if name =~ %r(^[+-.$()?*/&%!"'_,]+)
+        return if name =~ /^[-.0-9]+$/
         name_length = name.length
         score = [-36000.0, -400 * (name_length ** 1.5)].max.to_i
         csv << [name, nil, nil, score, '名詞', '一般', '*', '*', '*', '*', name, '*', '*']
